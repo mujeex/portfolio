9ba7e94c1a38f249cf624dfce51f7baa778faaa8
mujeex <mujahid.bappai@yahoo.com>
2019-05-07 23:57:48 +0800

    added rich-text-rendering and project page

--- src/components/blogComponents/blog-post.js
+++ src/components/blogComponents/blog-post.js
@@ -1,29 +1,49 @@
 import React from 'react'
 import { graphql} from 'gatsby'
+import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
 
 
-const blogPost=({pageContext})=>{
+const blogPost=({data,pageContext})=>{
+
+    console.log(data.contentfulBlog.content)
 
     return (
         <div>
-            {pageContext.slug}
+            {data.contentfulBlog.author}
+            {/* {pageContext.slug} */}
+            <div>
+            {documentToReactComponents(data.contentfulBlog.content.json)}
+            </div>
+            
             new page created
         </div>
     )
 }
 
 export default blogPost
-export const pageQuery= graphql`
-query ($slug: String!){
-    allContentfulBlog (filter:{slug:{eq: $slug }}) {
-        edges{ 
-          node{
-              slug
-              author
-           content{
-               content
-           }
-          }
+// export const pageQuery= graphql`
+// query ($slug: String!){
+//     allContentfulBlog (filter:{slug:{eq: $slug }}) {
+//         edges{ 
+//           node{
+//               slug
+//               author
+//            content{
+//                content
+//            }
+//           }
+//         }
+//       }
+// }
+// `
+
+export const query= graphql`
+query($slug: String!){
+    contentfulBlog(slug:{eq: $slug}){
+        author
+        slug
+        content{
+          json
         }
       }
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
+import styles from '../components/pageStyles/projectStyles.module.css'
 
 const project=()=>(
     <Layout>
-    <div>
-        project page
+    <div className={styles.projectsContainer}>
+
+    <article className={styles.projectContainer}>
+        <p className={styles.projectName}>Ice cream project</p>
+        <div className={styles.projectNumber}>1</div>
+    </article>
+
     </div>
     </Layout>
    
