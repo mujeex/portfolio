57441cdfc8301468725ab73db2c6155f40fbea1f
mujeex <mujahid.bappai@yahoo.com>
2019-05-09 14:10:51 +0800

    added options to rich-text-renderer

--- src/components/blogComponents/blog-post.js
+++ src/components/blogComponents/blog-post.js
@@ -1,42 +1,50 @@
 import React from 'react'
 import { graphql} from 'gatsby'
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
+import styles from './blog-post.module.css'
 
 
-const blogPost=({data,pageContext})=>{
+//component to customize bold marks and text nodes
+
+const Bold = ({ children }) => <p className={styles.boldType}>{children}</p>;
+
+const Text = ({ children }) => <p className="align-center">{children}</p>;
+
+// const CustomComponent = ({ file, title}) => (
+//      <img className={className} src={file.url} alt={title} />
+//   );
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: text => <Bold>{text}</Bold>,
+  },
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    [BLOCKS.EMBEDDED_ASSET]: node => {
+        return <img className={styles.images} src={node.data.target.fields.file['en-US'].url} alt='apple'/>
+      }
+  },
+  renderText: text => text.replace('!', '?'),
+};
+
+
+const blogPost=({data})=>{
 
     console.log(data.contentfulBlog.content)
 
     return (
-        <div>
+        <div className={styles.blogContainer}>
             {data.contentfulBlog.author}
-            {/* {pageContext.slug} */}
             <div>
-            {documentToReactComponents(data.contentfulBlog.content.json)}
+            {documentToReactComponents(data.contentfulBlog.content.json,options)}
             </div>
             
-            new page created
         </div>
     )
 }
 
 export default blogPost
-// export const pageQuery= graphql`
-// query ($slug: String!){
-//     allContentfulBlog (filter:{slug:{eq: $slug }}) {
-//         edges{ 
-//           node{
-//               slug
-//               author
-//            content{
-//                content
-//            }
-//           }
-//         }
-//       }
-// }
-// `
-
 export const query= graphql`
 query($slug: String!){
     contentfulBlog(slug:{eq: $slug}){
