a5272fb2d5f1b577447b57dcd32d71d2c11ef3c8
mujeex <mujahid.bappai@yahoo.com>
2019-05-12 13:04:08 +0800

    pages for project added

--- src/components/projectComponents/project.js
+++ src/components/projectComponents/project.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import {graphql} from 'gatsby'
+import styles from './project.module.css'
+import Layout from '../layout'
 
 const projectPage= ({data})=>{
+
+    const contents=data.contentfulProjects
     return(
-        <div>
-            {data.contentfulProjects.slug}
+        <Layout>
+             <div className={styles.container}>
+             <figure className={styles.projectPhotos}><figcaption>my picture goes here</figcaption></figure>
+             <article className={styles.projectStory}>
+             <div className={styles.storyHeader}>
+             <h2>Story</h2>
+             </div>
+               <div className={styles.storyContainer}>
+               <p>{contents.story.story}</p>
+               </div>
+                
+             </article>
+            <article className={styles.projectDetails}>
+            <h2>Details</h2>
+            <ul>
+                <li>role: {contents.role}</li>
+                <li>stack: {contents.stack}</li>
+                <li>niche: {contents.niche}</li>
+                <li>status: {contents.status}</li>
+                <li>client: {contents.client}</li>
+            </ul>
+            <div className={styles.buttonsContainer}>
+            <button><a href='www.github.com'>github</a></button>
+            <button><a href="www.github.com">Launch Site!</a></button>
+            </div>
+            
+            </article>
         </div>
+        </Layout>
+       
     )
 }
 
@@ -15,12 +46,15 @@ query($slug: String!){
     contentfulProjects (slug:{eq: $slug}){
         role
        stack
-       projectName
        client
        niche
        github
        site
+       story{
+           story
+       }
        siteType
        slug
+       status
        }
 }`
