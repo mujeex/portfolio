4b3b659f7b32045c74c9badfa9a5901378f53879
mujeex <mujahid.bappai@yahoo.com>
2019-08-01 17:14:06 +0800

    completed resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,71 @@
 import React from 'react'
 import Layout from '../components/layout'
+import styles from '../components/pageStyles/resumeStyles.module.css'
+import {graphql}from 'gatsby'
 
 
-const resume=()=>(
+const resume=({data})=>{
+    
+    data=data.contentfulResume.entry
+    return(
     <Layout>
  <div className={styles.resume_container}>
- <h1>Mujahid Bappai</h1>
+ <h1>{data.intro.heading}</h1>
+<p><b>{data.intro.content}</b>
+</p>
+
+<h2>{data.skills.heading}</h2>
+<ul>
+    {data.skills.content.map(skill=>(<li key={Math.random()}>{skill}</li>))}
+</ul>
+
+<h2>{data.work.heading}</h2>
+<ul>
+   {data.work.content.map(work=>(<li key={Math.random()}>{work}</li>))}
+</ul>
+
+<h2>{data.professional.heading}</h2>
+<ul>
+    {data.professional.content.map(prof=>(<li key={Math.random()}>{prof}</li>))}
+</ul>
+
+<h2>{data.honors.heading}</h2>
+<ul>
+    {data.honors.content.map(honor=>(<li key={Math.random()}>{honor}</li>))}
+</ul>
 
 </div>
     </Layout>
    
-)
+)}
 
 export default resume
+
+export const query=graphql`
+{
+    contentfulResume {
+      entry {
+            intro{
+                heading
+                content
+            }
+         skills{
+          heading
+          content
+        }
+        work {
+          content
+          heading
+        }
+        professional{
+            heading
+            content
+        }
+        honors{
+            heading
+            content
+        }
+      }
+    }
+  }
+`
