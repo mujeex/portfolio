b19df6ef40ec7b258a1fa21dac47516f61c7ebd1
mujeex <mujahid.bappai@yahoo.com>
2019-08-03 20:15:15 +0800

    added wechat modal

---
+++ src/components/contactComp/wechat.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './wechat.module.css'
+import Button from '../sharedComp/button/button'
+
+const modal=({wechatId})=>(
+    <article className={styles.modal}>
+        <Button>Back</Button>
+        <div className={styles.modal_content}>
+            <figure className={styles.modal_content__image}> 
+                <image className={styles.image} alt='wechatBar' src='../../images/barcode.JPG'></image>
+            </figure>
+            <div className={styles.modal_content__id}>
+            <h4>WechatID:</h4>
+            <p>{wechatId}</p>
+            </div>
+        </div>
+    </article>
+)
+
+export default modal
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from '../components/pageStyles/contactStyles.module.css'
 import Layout from '../components/layout'
+import {graphql} from 'gatsby'
+import Modal from '../components/contactComp/wechat'
 
-const contact=()=>(
+const contact=({data})=>{
+
+    const [modal,setModal]=useState(false)
+    
+    const infos= data.contentfulContact
+    return(
     <Layout>
          <div className={styles.contact_container}>
-
+        <Modal wechatId={infos.wechatId}/>
          <div className={styles.contact_content}>
             <h2>Hit me up if you ever have anything you want to talk about.</h2>
-            <ul><li><a href="">Facebook</a></li>
-            <li><a href="">Github</a></li>
-            <li><a href="">Wechat</a></li>
+            <ul><li><a target='_blank' href={`${infos.facebookLink}`}>Facebook</a></li>
+            <li><a target='_blank' href={`${infos.githubLink}`}>Github</a></li>
+            <li><a  href="">Wechat</a></li>
             </ul>
          </div>
     </div>
     </Layout>
    
-)
+)}
 
 export default contact
+export const query=graphql`
+{
+    contentfulContact{
+        githubLink
+        wechatId
+      }
+}`
